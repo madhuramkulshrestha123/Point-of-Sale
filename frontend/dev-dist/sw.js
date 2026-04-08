4e7ab6f8f6fe8b14741a41c3c17acaf07adc4a91
ojaswii <ojaswigahoi@gmail.com>
2026-04-09 00:53:57 +0530

    fixed

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rqv32b48vi8"
+    "revision": "0.ks3ck9ophpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
